cdec39ec2d4340caab5d4f6db42636a273ec4d48
Paramoshka <parfenov_ivan_42a@mail.ru>
2025-04-26 14:04:48 +0700

    [Implemnted function create thread_pool]
    Signed-off-by: Paramoshka <parfenov_ivan_42a@mail.ru>

--- src/thread_pool.c
+++ src/thread_pool.c
@@ -16,6 +16,53 @@ ThreadPool* create_thread_pool(int num_threads, int queue_size) {
     perror("Failed create thread pool in calloc");
     exit(EXIT_FAILURE);
   }
+
+  if (pthread_mutex_init(&thread_pool->queue_mutex, NULL) != 0) {
+    perror("Failed to initialize pool_thread mutex queue");
+    free(thread_pool);
+    exit(EXIT_FAILURE);
+  }
+
+  if (pthread_cond_init(&thread_pool->queue_cond, NULL) != 0) {
+    perror("Failed to initialize queue condvar");
+    pthread_mutex_destroy(&thread_pool->queue_mutex);
+    free(thread_pool);
+    exit(EXIT_FAILURE);
+  }
+
+  // Allocate memory for client
+  thread_pool->client_queue = calloc(queue_size, sizeof(int));
+  if (!thread_pool->client_queue) {
+    perror("Failed to allocate client queue");
+    pthread_mutex_destroy(&thread_pool->queue_mutex);
+    pthread_cond_destroy(&thread_pool->queue_cond);
+    free(thread_pool);
+    exit(EXIT_FAILURE);
+  }
+
+  // Allocate memory for threads
+  thread_pool->threads = calloc(num_threads, sizeof(pthread_t));
+  if (!thread_pool->threads) {
+    perror("Failed to allocate thread array");
+    free(thread_pool->client_queue);
+    pthread_mutex_destroy(&thread_pool->queue_mutex);
+    pthread_cond_destroy(&thread_pool->queue_cond);
+    free(thread_pool);
+    exit(EXIT_FAILURE);
+  }
+
+  // set threaed pool parameters
+
+  thread_pool->queue_front = 0;
+  thread_pool->queue_rear = 0;
+  thread_pool->queue_count = 0;
+  thread_pool->max_queue_size = queue_size;
+  thread_pool->thread_count = 0;
+  thread_pool->max_threads = num_threads;
+  thread_pool->keep_running = 1;
+
+
+  return thread_pool;
 }
 
 // static int queue_front = 0;

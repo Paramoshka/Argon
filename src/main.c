6937da4955cdd42d0c86537b3205a6ac3a2521c4
Paramoshka <parfenov_ivan_42a@mail.ru>
2025-05-03 20:15:44 +0700

    added parse config function and struct ServerConfig
    Signed-off-by: Paramoshka <parfenov_ivan_42a@mail.ru>

--- src/main.c
+++ src/main.c
@@ -1,7 +1,20 @@
 #include "../include/server.h"
 #include "../include/http.h"
+#include "../include/config.h"
+
+
+
+const char * cfg_path = "argon.conf";
 
 int main() {
+  ServerConfig* cfg = calloc(1, sizeof(ServerConfig));
+  if (parse_config(cfg_path, cfg) == -1)
+  {
+    perror("Read conf failed");
+    return 1;
+  }
+  print_config(cfg);
+  
   Server* server = server_init("0.0.0.0", 8080, handle_client);
   server_run(server);
   server_shutdown(server);

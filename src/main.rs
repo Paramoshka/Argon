b9a343105d59e46d4cd6fc20e61b8b2273f37fc3
Paramoshka <parfenov_ivan_42a@mail.ru>
2025-06-12 11:46:45 +0300

    ruster
    Signed-off-by: Paramoshka <parfenov_ivan_42a@mail.ru>

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 use crate::config::ServerConfig;
+use crate::server::Server;
 
 mod config;
 mod server;
 
-fn main() {
-    let server = ServerConfig::new().expect("Could not create server config");
-    println!("{}", server[0].host)
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let server_config_list = ServerConfig::new().expect("Could not create server config");
+    for server_config in server_config_list {
+        let server = Server::new(server_config).await?;
+        server.run().await?;
+    }
+    
+    Ok(())
 }
